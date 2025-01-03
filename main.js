2c85b24b35f756572a0b9c00a0a344ad82470f19
Laimismk <laimis.mik@gmail.com>
2025-01-03 15:47:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ const detectCode = () => {
     for (const barcode of codes)  {
       // Log the barcode to the console
       console.log(barcode)
+      document.getElementById("result").textContent = "Nuskaitytas tekstas lllll"
     }
   }).catch(err => {
     // Log an error if one happens
