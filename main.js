94780285afb6d12af9f99ea5f136b48133978f51
Laimismk <laimis.mik@gmail.com>
2025-01-03 15:58:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,8 @@ const detectCode = () => {
     for (const barcode of codes)  {
       // Log the barcode to the console
       console.log(barcode)
-      // Show message "NUSKAITYTA" on the screen
+      // Show message with the barcode content
+      messageElement.innerText = `NUSKAITYTA: ${barcode.rawValue}`;  // Display the decoded value
       messageElement.style.display = 'block';  // Display the message
       setTimeout(() => {
         messageElement.style.display = 'none'; // Hide message after 2 seconds
